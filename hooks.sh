e95e85ed8fcc3fe26dcf43bb6a0c6e208ae1ff1e
esteban alvarado <estalvgs1999@gmail.com>
2021-08-18 21:43:33 -0600

    JIRA-000 [test] Automated navigation test case

--- hooks.sh
+++ hooks.sh
@@ -1,6 +1,17 @@
-#!/bin/sh
+#!/bin/bash -l
 
-if ! egrep "^#.copyright" */*.py; then
+# Check for commit message structure
+word="JIRA-[0-9]+"
+isPresent=$(grep -Eoh "$word" $1)
+
+if [[ -z $isPresent ]]
+  then echo "Commit message KO, $word is missing"; exit 1;
+  else echo "Commit message OK"; exit 0;
+fi
+
+# Check for copyright comment in *.py filess
+if [[ ! `egrep "^#.*copyright.*$" */*.py` ]]; then
     echo "ERROR: Disallowed development configuration in file: .py"
     exit 1
 fi
+
