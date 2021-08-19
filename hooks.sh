38a1a343a5e8574cfedc952c3e4edf594f25f7eb
esteban alvarado <estalvgs1999@gmail.com>
2021-08-18 22:04:44 -0600

    JIRA-000 [add] Bash script

--- hooks.sh
+++ hooks.sh
@@ -1,14 +1,5 @@
 #!/bin/bash -l
 
-# Check for commit message structure
-word="JIRA-[0-9]+"
-isPresent=$(grep -Eoh "$word" $1)
-
-if [[ -z $isPresent ]]
-  then echo "Commit message KO, $word is missing"; exit 1;
-  else echo "Commit message OK"; exit 0;
-fi
-
 # Check for copyright comment in *.py filess
 if [[ ! `egrep "^#.*copyright.*$" */*.py` ]]; then
     echo "ERROR: There is no copyright comment in the file: : .py"
@@ -33,3 +24,11 @@ if [[ ! `egrep "^//.*copyright.*$" */*.java` ]]; then
     exit 1
 fi
 
+# Check for commit message structure
+word="JIRA-[0-9]+"
+isPresent=$(grep -Eoh "$word" $1)
+
+if [[ -z $isPresent ]]
+  then echo "Commit message KO, $word is missing"; exit 1;
+  else echo "Commit message OK"; exit 0;
+fi
